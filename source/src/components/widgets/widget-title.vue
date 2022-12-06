4e16fd1e07db7bb7deb82bf88afa70a799b08cbc
Scott <iscottt@163.com>
2022-12-06 22:06:46 +0800

    feat: 🎉标题间距

--- source/src/components/widgets/widget-title.vue
+++ source/src/components/widgets/widget-title.vue
@@ -16,6 +16,7 @@ export default defineComponent({
   border-bottom: 1px solid var(--border-color);
   box-shadow: 0 1px var(--white-opacity-7);
   color: #5a6f87;
+  margin-bottom: 40px;
   span {
     display: inline-block;
     position: relative;
