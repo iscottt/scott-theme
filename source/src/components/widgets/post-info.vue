389edac3f824d649f65ad582aed916cae20b42b5
Scott <1194379729@qq.com>
2022-12-16 10:35:39 +0800

    feat: 🎉修改标签

--- source/src/components/widgets/post-info.vue
+++ source/src/components/widgets/post-info.vue
@@ -2,8 +2,8 @@
   <div class="widget post-info">
     <widgetTitle>标签与分类</widgetTitle>
     <div class="tags">
-      <router-link v-for="item in post.taxonomies.tag" :to="`/tag/${item.slug}`"> <svg-icon name="tag" :height="14" /> {{ item.name }} </router-link>
-      <router-link v-for="item in post.taxonomies.category" :to="`/category/${item.slug}`"> <svg-icon name="book" :height="14" /> {{ item.name }} </router-link>
+      <router-link v-for="item in post.taxonomies.tag" :to="`/tag/${item.slug}`"> <i class="fa-solid fa-tags"></i> {{ item.name }} </router-link>
+      <router-link v-for="item in post.taxonomies.category" :to="`/category/${item.slug}`"> <i class="fa-solid fa-graduation-cap"></i> {{ item.name }} </router-link>
     </div>
   </div>
 </template>
@@ -30,12 +30,10 @@ export default defineComponent({
   a {
     z-index: 0;
     text-decoration: none;
-    border: 1px solid;
     border-radius: 0.375rem;
     font-size: 87.5%;
     color: #73869a;
-    border-color: rgba(54, 100, 152, 0.15);
-    background-color: #ecf2fb;
+    background-color: #fff;
     text-shadow: 0 1px var(--white-default);
     padding: 0.25em 0.9em 0.25em 0.6em;
     border-radius: 0.5em;
@@ -48,9 +46,7 @@ export default defineComponent({
     &:hover {
       color: #007dff;
       text-shadow: 0.1326rem 0.1326rem 0.1875rem rgb(0 125 255 / 50%), -1px -1px 1px hsl(0deg 0% 100% / 80%);
-      border-color: transparent;
-      box-shadow: min(-1px, -0.0625rem) min(-1px, -0.0625rem) hsla(0, 0%, 100%, 0.6), max(1px, 0.0625rem) max(1px, 0.0625rem) rgba(54, 100, 152, 0.2),
-        inset 0.1875rem 0.1875rem 0.1875rem rgba(54, 100, 152, 0.2), inset -0.125rem -0.125rem 0.1875rem hsla(0, 0%, 100%, 0.6);
+      box-shadow: 0px 10px 15px -10px rgb(0 0 0 / 45%);
     }
   }
 }
